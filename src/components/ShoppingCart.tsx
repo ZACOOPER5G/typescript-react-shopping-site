10be1e844daf6651ce9c512b14624c6a7cb13d55
zac <zacooper5g@outlook.com>
2022-08-29 11:28:04 +1000

    added shopping cart slide out menu

---
+++ src/components/ShoppingCart.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Offcanvas } from 'react-bootstrap'
+import { useShoppingCart } from '../context/ShoppingCartContext'
+
+type ShoppingCartProps = {
+    isOpen: boolean;
+}
+
+export const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
+  const { closeCart } = useShoppingCart()
+
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+        <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Cart</Offcanvas.Title>
+        </Offcanvas.Header>
+    </Offcanvas>
+  )
+}
