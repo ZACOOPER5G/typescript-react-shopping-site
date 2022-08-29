4295418323f0fad1f63e6f40cf32fd4e41f8baba
zac <zacooper5g@outlook.com>
2022-08-29 11:27:36 +1000

    updated navbar cart button to open and close cart, as well as have a dynamic display for cart items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
 import { Button, Container, Nav, Navbar as NavbarBs } from 'react-bootstrap'
 import { NavLink } from 'react-router-dom'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 
 export const Navbar = () => {
+  const { openCart, cartQuantity } = useShoppingCart();
+
   return (
       <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
           <Container>
@@ -11,9 +13,12 @@ export const Navbar = () => {
                   <Nav.Link to='./store' as={NavLink} >Store</Nav.Link>
                   <Nav.Link to='./about' as={NavLink} >About</Nav.Link>
               </Nav>
-              <Button className="cart-button">
-                  <p className="cart-title">Cart</p>
-                  <div className="cart-counter rounded-circle bg-danger d-flex justify-content-center align-items-center">3</div>
+              <Button 
+                className="cart-button" 
+                onClick={openCart}
+              >
+                  <p className="cart-title" >Cart</p>
+                  <div className="cart-counter rounded-circle bg-danger d-flex justify-content-center align-items-center">{cartQuantity}</div>
               </Button >
           </Container>
       </NavbarBs>
