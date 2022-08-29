1fd28c7738ba753c5c1361516c97db54db837b39
zac <zacooper5g@outlook.com>
2022-08-29 12:03:07 +1000

    cart items menu now shows cart items

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import storeItems from '../data/items.json'
+import { formatCurrency } from "../utilities/formatCurrency";
 
-export const CartItem = () => {
+type CartItemProp = {
+    id: number;
+    quantity: number;
+}
+
+export const CartItem = ({ id, quantity }: CartItemProp) => {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find(i => i.id === id)
+  if (item === null) return null
+
   return (
-    <div>CartItem</div>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center" >
+        <img className="shopping-cart-img" src={item!.imgUrl} />
+        <div className="me-auto ">
+            <div>
+                {item!.name} {quantity > 1 && <span className="text-muted cart-item-quantity">x{quantity}</span>}
+            </div>
+            <div className="text-muted cart-item-pricing">
+                {formatCurrency(item!.price)}
+            </div>
+        </div>
+        <div className="me-auto">
+            {formatCurrency(item!.price * quantity)}
+        </div>
+        <Button variant="outline-danger" size="sm" onClick={() => removeFromCart(item!.id)}>&times;</Button>
+    </Stack>
   )
 }
